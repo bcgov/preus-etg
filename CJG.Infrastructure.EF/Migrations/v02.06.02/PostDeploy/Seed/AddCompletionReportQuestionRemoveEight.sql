e637fc38eab16599fe2adc386b1a6b875f6ab7ce
Will Weinzettl <will.weinzettl@number41media.com>
2022-09-28 14:56:27 -0700

    CJG-988 Completion Report questions updated from 8 to 1

---
+++ CJG.Infrastructure.EF/Migrations/v02.06.02/PostDeploy/Seed/AddCompletionReportQuestionRemoveEight.sql
@@ -0,0 +1,20 @@
+﻿PRINT 'Starting Update of ETG Completion Report Questions'
+
+PRINT 'Inserting [Completion Report Question]'
+
+UPDATE [CompletionReportQuestions]
+SET IsActive = 0,
+DateUpdated = GETUTCDATE()
+WHERE CompletionReportId = 1
+AND GroupId = 4
+  
+PRINT 'Deactivating Existing [Completion Report Questions]'
+
+INSERT INTO [CompletionReportQuestions] ([CompletionReportId], [Question], Description, Audience, GroupId, [Sequence], IsRequired, IsActive, QuestionType, DefaultText, DateAdded, DisplayOnlyIfGoto)
+VALUES (1, 
+'Do you have any suggestions on how we could improve the Employer Training Grant Program?<br/>For example, was the website clear; was the online application easy to complete and submit; was the Eligibility Criteria clear and easy to follow?',
+'Open-ended question for ETG Employers', 1, 4, 1, 1, 1, 3, 
+'<p><strong>Please take a few minutes to complete the survey question. Your response will help improve future services provided by the Employer Training Grant program:</strong></p><h3>Opportunity to Provide Feedback</h3>', 
+GETUTCDATE(), 0)
+
+PRINT 'Done Update of ETG Completion Report Questions'
--- CJG.Web.External/Areas/Ext/Models/Reports/CompletionReportGroupViewModel.cs
+++ CJG.Web.External/Areas/Ext/Models/Reports/CompletionReportGroupViewModel.cs
@@ -1,25 +1,21 @@
-﻿using CJG.Application.Services;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CJG.Application.Services;
 using CJG.Core.Entities;
 using CJG.Core.Interfaces.Service;
 using CJG.Web.External.Models.Shared;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace CJG.Web.External.Areas.Ext.Models.Reports
 {
-	public class CompletionReportGroupViewModel : BaseViewModel
+    public class CompletionReportGroupViewModel : BaseViewModel
 	{
-		#region Properties
 		public string Title { get; set; }
 		public int GrantApplicationId { get; set; }
 		public bool SaveOnly { get; set; } = false;
 
 		public IEnumerable<CompletionReportQuestionViewModel> Questions { get; set; }
-		#endregion
 
-		#region Constructors
 		public CompletionReportGroupViewModel()
 		{
 		}
@@ -33,15 +29,15 @@ namespace CJG.Web.External.Areas.Ext.Models.Reports
 			if (completionReportGroup == null) throw new ArgumentNullException(nameof(completionReportGroup));
 
 			Utilities.MapProperties(completionReportGroup, this);
-			this.GrantApplicationId = grantApplication.Id;
+			GrantApplicationId = grantApplication.Id;
 
-			this.Questions = completionReportGroup.Questions
+			Questions = completionReportGroup.Questions
 				.Where(x => x.CompletionReportId == grantApplication.CompletionReportId)
+				.Where(x => x.IsActive)
 				.OrderBy(x => x.Sequence)	// Because we are adding a CWRG report, we need to start using Sequence
 				.ThenBy(x => x.Id)			// The original ETG report did not apply sequence everywhere so order by Id as well.
-				.Select(o =>
-				new CompletionReportQuestionViewModel(grantApplication, o, naIndustryClassificationSystemService, nationalOccupationalClassificationService)).ToArray();
+				.Select(o => new CompletionReportQuestionViewModel(grantApplication, o, naIndustryClassificationSystemService, nationalOccupationalClassificationService))
+				.ToArray();
 		}
-		#endregion
 	}
 }
