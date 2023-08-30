451966d7cb20629ee2f88273a05dd488c35a8003
Will Weinzettl <will.weinzettl@number41media.com>
2023-08-30 14:07:00 -0700

    CJG-1398 Fixed claim participant name formatting

--- CJG.Web.External/Areas/Int/Models/Claims/ParticipantViewModel.cs
+++ CJG.Web.External/Areas/Int/Models/Claims/ParticipantViewModel.cs
@@ -1,29 +1,26 @@
-﻿using CJG.Core.Entities;
-using System;
+﻿using System;
+using CJG.Core.Entities;
 
 namespace CJG.Web.External.Areas.Int.Models.Claims
 {
 	public class ParticipantViewModel
 	{
-		#region Properties
 		public int ParticipantFormId { get; set; }
 		public string Name { get; set; }
 		public string Email { get; set; }
 		public string PhoneNumber { get; set; }
-		#endregion
 
-		#region Constructors
 		public ParticipantViewModel() { }
 
 		public ParticipantViewModel(ParticipantForm participant)
 		{
-			if (participant == null) throw new ArgumentNullException(nameof(participant));
+			if (participant == null)
+				throw new ArgumentNullException(nameof(participant));
 
-			this.ParticipantFormId = participant.Id;
-			this.Name = $"{participant.FirstName} {participant.LastName}";
-			this.Email = participant.EmailAddress;
-			this.PhoneNumber = $"{participant.PhoneNumber1} {participant.PhoneExtension1}";
+			ParticipantFormId = participant.Id;
+			Name = $"{participant.LastName}, {participant.FirstName}";
+			Email = participant.EmailAddress;
+			PhoneNumber = $"{participant.PhoneNumber1} {participant.PhoneExtension1}";
 		}
-		#endregion
 	}
 }
