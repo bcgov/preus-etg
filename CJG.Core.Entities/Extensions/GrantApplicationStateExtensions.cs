ad8d207ac2ad5620c26e25959c096b9c362348c2
Will Weinzettl <will.weinzettl@number41media.com>
2026-04-29 15:52:00 -0700

    CJG-1715 Training Costs are set when ReturnedToNew to Selected

--- CJG.Core.Entities/Extensions/GrantApplicationStateExtensions.cs
+++ CJG.Core.Entities/Extensions/GrantApplicationStateExtensions.cs
@@ -24,5 +24,24 @@ namespace CJG.Core.Entities.Extensions
 
 			return true;
 		}
+
+		public static bool HasBeenReturnedToNew(this GrantApplication grantApplication)
+		{
+			if (grantApplication?.StateChanges == null || !grantApplication.StateChanges.Any())
+				return false;
+
+			if (grantApplication.StateChanges.Count == 1)
+				return false;
+
+			var newStates = grantApplication.StateChanges
+				.Where(sc => sc.ToState == ApplicationStateInternal.New)
+				.OrderByDescending(sc => sc.ChangedDate)
+				.ToList();
+
+			if (grantApplication.StateChanges.Count <= 1)
+				return false;
+
+			return newStates.Any(state => state.FromState != ApplicationStateInternal.Draft);
+		}
 	}
 }
